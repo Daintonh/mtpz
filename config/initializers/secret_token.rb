c84b95ce3ece6868a69a10ff477842b6916736da
veryona <info@veryona.com>
2012-04-04 15:10:11 -0400

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mixtapez::Application.config.secret_token = '3bb5383747d84139ef7664a1a6dabe9fb4300e142d0e51143760a1f686e60a348be7d2f3f73d25876680985e234854941a261c9d898404081f4a5f2b39ba2cb9'
